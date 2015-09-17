2958eb27e636a93939996cebae61fef6024f184a
Optimised server <support@eskimosoup.co.uk>
2015-09-17 15:06:55 +0100

    Snags: page names in nav, logo link and team members

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,6 +1,6 @@
 class TeamMembersController < ApplicationController
   def index
     @additional_content = AdditionalContentPresenter.new(object: AdditionalContent.find_by(area: 'Team Members index'), view_template: view_context)
-    @team_members = TeamMember.displayable.group_by(&:job_title)
+    @team_members = BaseCollectionPresenter.new(collection: TeamMember.displayable, view_template: view_context, presenter: TeamMemberPresenter)
   end
 end
